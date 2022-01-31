319618553fc9280e0453cfda35f1566df18984a1
blagoev <lubo@blagoev.com>
2022-01-31 11:17:02 +0200

    update realm_dart sample

--- realm_dart/bin/myapp.dart
+++ realm_dart/bin/myapp.dart
@@ -22,39 +22,50 @@ part 'myapp.g.dart';
 
 @RealmModel()
 class _Car {
-  late String make;
+  late final String make;
+  String? model;
+  int? kilometers = 500;
+  _Person? owner;
 }
 
 @RealmModel()
 class _Person {
   late String name;
+  int age = 1;
 }
 
 void main(List<String> arguments) {
   var config = Configuration([Car.schema, Person.schema]);
   var realm = Realm(config);
 
+  var myCar = Car("Tesla", model: "Model Y", kilometers: 1);
   realm.write(() {
-    print('Creating Realm object of type Car');
-    var car = realm.add(Car('Audi'));
-    print('The car is ${car.make}');
+    print('Adding a Car to Realm.');
+    var car = realm.add(Car("Tesla", owner: Person("John")));
+    print("Updating the car's model and kilometers");
+    car.model = "Model 3";
+    car.kilometers = 5000;
 
-    car.make = 'VW';
-    print('The car is ${car.make}');
+    print('Adding another Car to Realm.');
+    realm.add(myCar);
+
+    print("Changing the owner of the car.");
+    myCar.owner = Person("me", age: 18);
+    print("The car has a new owner ${car.owner!.name}");
   });
 
-  var allCars = realm.all<Car>();
-  var indexedCar = allCars[0];
-  print('The indexedCar is ${indexedCar.make}');
+  print("Getting all cars from the Realm.");
+  var cars = realm.all<Car>();
+  print("There are ${cars.length} cars in the Realm.");
 
-  realm.write(() {
-    print('Creating more cars');
-    var car = realm.add(Car('Audi'));
-    print('The car is ${car.make}');
-  });
+  var indexedCar = cars[0];
+  print('The first car is ${indexedCar.make} ${indexedCar.model}');
 
-  var filteredCars = realm.all<Car>().query("make == 'Audi'");
-  print('Found ${filteredCars.length} Audi cars');
+  print("Getting all Tesla cars from the Realm.");
+  var filteredCars = realm.all<Car>().query("make == 'Tesla'");
+  print('Found ${filteredCars.length} Tesla cars');
 
-  print('Done');
+  realm.close();
+
+  print("Done");
 }
