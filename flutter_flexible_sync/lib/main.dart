11e546335da370e7becad0def7a04a5b64c5c925
blagoev <lubo@blagoev.com>
2022-07-08 14:52:38 +0300

    add netrowk client entitlements

--- flutter_flexible_sync/lib/main.dart
+++ flutter_flexible_sync/lib/main.dart
@@ -126,13 +126,10 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               const Text('Important tasks count:', style: TextStyle(fontWeight: FontWeight.bold)),
               Text('$_importantTasksCount', style: Theme.of(context).textTheme.headline4),
-              Text('Realm path: ${MyApp.importantTasksRealm.config.path}'),
               const Text('Normal tasks count:', style: TextStyle(fontWeight: FontWeight.bold)),
               Text('$_normalTasksCount', style: Theme.of(context).textTheme.headline4),
-              Text('Realm path: ${MyApp.normalTasksRealm.config.path}'),
               const Text('All tasks count:', style: TextStyle(fontWeight: FontWeight.bold)),
               Text('$_allTasksCount', style: Theme.of(context).textTheme.headline4),
-              Text('Realm path: ${MyApp.allTasksRealm.config.path}'),
             ],
           ),
         ),
