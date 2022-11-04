5ec864ad36167d446ae1a6f75293b58ca9c2a21a
Kasper Overgård Nielsen <kasper@byolimit.com>
2022-11-04 12:34:40 +0100

    Suggestions (#17)
    Demonstrate how to use realm in Flutter UI using `StreamBuilder`

--- bundled_realm/lib/main.dart
+++ bundled_realm/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:realm/realm.dart';
 import 'package:flutter/services.dart';
@@ -15,80 +16,70 @@ void main() async {
 Future<Realm> initRealm(String assetKey) async {
   final config = Configuration.local([Car.schema]);
   final file = File(config.path);
+  // await file.delete(); // <-- uncomment this to start over on every restart
   if (!await file.exists()) {
     ByteData realmBytes = await rootBundle.load(assetKey);
-    await file.writeAsBytes(realmBytes.buffer.asUint8List(realmBytes.offsetInBytes, realmBytes.lengthInBytes), mode: FileMode.write);
+    await file.writeAsBytes(
+      realmBytes.buffer.asUint8List(realmBytes.offsetInBytes, realmBytes.lengthInBytes),
+      mode: FileMode.write,
+    );
   }
   return Realm(config);
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+
+  void _addRandomCar() {
+    const cars = ['Mercedes', 'VW', 'Ford', 'Tesla', 'Ferrari', 'Polestar', 'Volvo', 'BMW'];
+    final r = Random();
+
+    realm.write(() {
+      realm.add(
+        Car(
+          cars[r.nextInt(cars.length)],
+          model: '${r.nextInt(20) + 2002}',
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late List<String> carsList = [];
-  String text = "";
-
-  void _incrementCars() {
-    setState(() {
-      realm.write(() => realm.add(Car("New make", model: "${carsList.length + 1}")));
-      text = getCarsList(realm);
+      );
     });
   }
 
-  String getCarsList(Realm realm) {
-    carsList.clear();
-    var cars = realm.all<Car>();
-    for (var car in cars) {
-      carsList.add("Car:${car.make},${car.model}");
-    }
-    return carsList.join("\n");
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              text,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: _addRandomCar,
+          tooltip: 'Add a car',
+          child: const Icon(Icons.add),
+        ),
+        body: StreamBuilder<RealmResultsChanges<Car>>(
+          stream: realm.all<Car>().changes,
+          builder: (context, snapshot) {
+            final data = snapshot.data;
+
+            if (data == null) return const CircularProgressIndicator();
+
+            final results = data.results;
+            return ListView.builder(
+              itemCount: results.length,
+              itemBuilder: (context, index) {
+                final c = results[index];
+                return ListTile(
+                  leading: const CircleAvatar(child: Icon(Icons.car_rental)),
+                  title: Text(c.make),
+                  subtitle: Text(c.model ?? ''),
+                );
+              },
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCars,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+    // home: const MyHomePage(title: 'Flutter Demo Home Page'));
   }
 }
