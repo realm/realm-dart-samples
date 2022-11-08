69f61256f0b6683ecd15d8f48ca44be3998d1924
Desislava Stefanova <dst.stefanova@gmail.com>
2022-11-08 16:48:47 +0200

    Update realm

--- bundled_realm/lib/model.g.dart
+++ bundled_realm/lib/model.g.dart
@@ -6,36 +6,39 @@ part of 'model.dart';
 // RealmObjectGenerator
 // **************************************************************************
 
-class Car extends _Car with RealmEntity, RealmObject {
+class Car extends _Car with RealmEntity, RealmObjectBase, RealmObject {
   Car(
     String make, {
     String? model,
   }) {
-    RealmObject.set(this, 'make', make);
-    RealmObject.set(this, 'model', model);
+    RealmObjectBase.set(this, 'make', make);
+    RealmObjectBase.set(this, 'model', model);
   }
 
   Car._();
 
   @override
-  String get make => RealmObject.get<String>(this, 'make') as String;
+  String get make => RealmObjectBase.get<String>(this, 'make') as String;
   @override
-  set make(String value) => RealmObject.set(this, 'make', value);
+  set make(String value) => RealmObjectBase.set(this, 'make', value);
 
   @override
-  String? get model => RealmObject.get<String>(this, 'model') as String?;
+  String? get model => RealmObjectBase.get<String>(this, 'model') as String?;
   @override
-  set model(String? value) => RealmObject.set(this, 'model', value);
+  set model(String? value) => RealmObjectBase.set(this, 'model', value);
 
   @override
   Stream<RealmObjectChanges<Car>> get changes =>
-      RealmObject.getChanges<Car>(this);
+      RealmObjectBase.getChanges<Car>(this);
+
+  @override
+  Car freeze() => RealmObjectBase.freezeObject<Car>(this);
 
   static SchemaObject get schema => _schema ??= _initSchema();
   static SchemaObject? _schema;
   static SchemaObject _initSchema() {
-    RealmObject.registerFactory(Car._);
-    return const SchemaObject(Car, 'Car', [
+    RealmObjectBase.registerFactory(Car._);
+    return const SchemaObject(ObjectType.realmObject, Car, 'Car', [
       SchemaProperty('make', RealmPropertyType.string),
       SchemaProperty('model', RealmPropertyType.string, optional: true),
     ]);
