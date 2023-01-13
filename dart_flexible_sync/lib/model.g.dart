d301e28faeae48ca1e96881b57844d15e7e45abc
Realm CI <realm-tools@mongodb.com>
2023-01-13 19:02:48 +0100

    Update samples with realm 0.9.0+rc (#38)

--- dart_flexible_sync/lib/model.g.dart
+++ dart_flexible_sync/lib/model.g.dart
@@ -55,7 +55,7 @@ class Task extends _Task with RealmEntity, RealmObjectBase, RealmObject {
   static SchemaObject _initSchema() {
     RealmObjectBase.registerFactory(Task._);
     return const SchemaObject(ObjectType.realmObject, Task, 'Task', [
-      SchemaProperty('_id', RealmPropertyType.objectid,
+      SchemaProperty('id', RealmPropertyType.objectid,
           mapTo: '_id', primaryKey: true),
       SchemaProperty('title', RealmPropertyType.string),
       SchemaProperty('status', RealmPropertyType.string),
