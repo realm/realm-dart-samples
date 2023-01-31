aeb5068b476336009a738c83c8a3fe9ea64fb65c
realm-ci <realm-ci@users.noreply.github.com>
2023-01-31 00:28:58 +0000

    Update samples with realm 0.11.0+rc

--- flutter_flexible_sync/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_flexible_sync/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_macos
+import path_provider_foundation
 import realm
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
