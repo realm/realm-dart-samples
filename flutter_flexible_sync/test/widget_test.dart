18782854beaf032bcef04b10cf04f5be7216314a
Desislava Stefanova <dst.stefanova@gmail.com>
2023-03-01 23:02:32 +0200

    Temp path added

--- flutter_flexible_sync/test/widget_test.dart
+++ flutter_flexible_sync/test/widget_test.dart
@@ -5,16 +5,22 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_flexible_sync/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:path/path.dart' as _path;
+import 'package:realm/realm.dart';
 
 void main() {
   setUp(() async {
+    final path = _path.join(Directory.systemTemp.createTempSync("realm_test_").path, "default_tests.realm");
+    Configuration.defaultRealmPath = path;
     await initRealm();
+    addTearDown(() async {
+      Realm.deleteRealm(path);
+    });
   });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
